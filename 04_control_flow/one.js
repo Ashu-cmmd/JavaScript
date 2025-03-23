f8d9b8c88f15d5e9444c3bfffc52fb40cae6c331
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-03-23 15:31:32 +0000

    #controlFlow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,63 @@
+// if
+// if (true) {
+
+// }
+// if (2==2) {
+
+// }
+
+const isUserLoggedIn = true
+
+const temperture = 41
+
+// if (isUserLoggedIn) {
+
+// }
+
+// < , >, <= ,>= ,== , != ,=== ,!==
+
+// if (temperture < 50) {
+//     console.log("less than 50");
+// }
+// else{
+// console.log("temperature greater than 50");
+// }
+
+// const score =200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power :  ${ power}`);
+// }
+// console.log(`User power :  ${ power}`);
+
+const balance = 1000
+
+// if (balance >500) console.log("test"),console.log("test2"); // NOT do it not preferable
+
+// if (balance < 500) {
+//     console.log("less than");
+    
+// }else if (balance < 750) {
+//     console.log("less than 750");
+    
+// }else if (balance < 900) {
+//     console.log("Less than 900");
+    
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+const UserLoggedIn = true
+const debitCard = true
+const LoggedInFromGoogle =false
+const LoggedInFromEmail = true
+if (UserLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");   
+}
+
+if (LoggedInFromEmail || LoggedInFromGoogle) {
+    console.log("User logged In");
+}
+
