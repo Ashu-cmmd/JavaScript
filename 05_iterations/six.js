dc36af23935c6fc74b38a948ec5c2493abb111f4
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-03-26 18:22:38 +0000

    fillter done

---
+++ 05_iterations/six.js
@@ -0,0 +1,33 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+// const values = coding.forEach( (item) => {
+//    // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums =[1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) => { // In arrow function if you use scope then write return 
+//     return num > 4
+// } )
+
+
+// const newNums =[]
+
+// myNums.forEach( (num) =>{
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+
+// let userBooks = book.filters ( (bk) => bk.genre === 'history')
+
+
+//  userBooks = book.filters ( (bk) => {return bk.publish >= 2000})
+
+
